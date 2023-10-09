56e27d2f517f86050fc38ced669d60bdc7e57c7e
Nero <jesalemalo@gmail.com>
2023-10-09 08:46:12 -0400

    I did make a view GETOne for MyUser

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, include
-from .views import ApiGetUserView
+from .views import ApiGetUserView, ApiGetOneUserView
 
 
 urlpatterns = [
     path("users/", ApiGetUserView.as_view(), name='users'),
-
+    path("users/<int:pk>/", ApiGetOneUserView.as_view(), name='user'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -21,4 +21,29 @@ class ApiGetUserView(APIView):
         else:
             return Response({"message":"Users not found"}, status=status.HTTP_404_NOT_FOUND)
         
+
+class ApiGetOneUserView(APIView):
+
+    def get_user(self, pk, user = None):
+        try:
+            user = MyUser.objects.get(pk=pk)
+            if user is not None:
+                return user
+            else:
+                return None
+        except MyUser.DoesNotExist:
+            raise("El usuario no existe")
+            
+        
+
+    def get(self, request, pk, format = None, user = None):
+        user = self.get_user(pk=pk)
+        user_serializer = MyUserSerializerGET(user)
+
+        if user is not None:
+            return Response(user_serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"message":"User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+
     
