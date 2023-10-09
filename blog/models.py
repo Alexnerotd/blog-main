ce030f090e5ccb4314d41482cca8e10c515adfac
Nero <jesalemalo@gmail.com>
2023-10-08 21:40:02 -0400

    i did create model Blog in blog folder

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from authentication.models import MyUser
 
 # Create your models here.
+
+
+class Blog(models.Model):
+    title = models.CharField("Titulo del blog", max_length=50, unique=True)
+    description = models.CharField("Descripcion del blog", max_length=100)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(MyUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.title}"
+    
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = "Blog"
+        verbose_name_plural = "Blogs"
+    
