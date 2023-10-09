180c4bf7ff84cfd59d017a44139fe52b52f167bb
Nero <jesalemalo@gmail.com>
2023-10-09 08:09:49 -0400

    I did make a modelserializer(GET)authentication.py

---
+++ authentication/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+
+from .models import MyUser
+
+
+class MyUserSerializerGET(ModelSerializer):
+
+    class Meta:
+        model = MyUser
+        fields = ['id', 'username', 'email', 'email']
+
+    def create(self, validate_data):
+        user = MyUser(**validate_data)
+        user.set_password(validate_data['password'])
+        user.save()
+        return user
